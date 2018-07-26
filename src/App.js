0efa27c71307fd741b96f14f8ab10ce8396b82d1
Barry Lam <blam452@gmail.com>
2018-07-25 22:59:55 -0400

    fixed some css issues, added link to download resume

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Card,
         Collapse,
         Col, 
         CardText, 
-        CardTitle, 
+        CardTitle,
+        CardFooter, 
         Progress } from 'reactstrap'
 
 class App extends Component {
@@ -21,45 +22,35 @@ class App extends Component {
     }
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   // console.log(prevState)
-  //   if(prevState){
-  //     console.log('prevState is', prevState)
-      
-  //   }
-  // }
-
   toggle(e) {
-    // let event = e.target.dataset.event;
-    
     let event = e.target.dataset.event
     if(!event) {
       console.log('event not working')
     } else {
-      console.log('event working!')
+      // console.log('event working!')
       this.setState({ 
           collapse:this.state.collapse === Number(event) ? 0 : Number(event)
-        })
+        }, () => console.log(event))
       }     
     }
   
 
   render() {
     const { collapse } = this.state;
-    console.log(collapse)
+    // console.log(collapse)
     return (
         <div className="App">
           <Container>
             <header className="main-header">
               <Row noGutters={true}>
                 <Col lg="4" md="5">
-                  <img src={require("./img/person1.jpg")} alt=""/>
+                  <img src={require("./img/person1.jpg")} alt="" className="img-fluid"/>
                 </Col>
                 <Col lg="8" md="7">
                   <div className="d-flex flex-column">
                     <div className="p-5 bg-dark text-white">
                       <div className="d-flex flex-row justify-content-between align-items-center">
-                        <h1 className=".display-4">Barry Lam</h1>
+                        <h1 className="display-4">Barry Lam</h1>
                         
                         <div className="d-none d-md-block">
                           <a href="http://twitter.com" className="text-white">
@@ -85,40 +76,69 @@ class App extends Component {
                     </div>
 
                     <div className="p-4 bg-black">
-                       Front-End Developer
+                       <h3>Front-End Developer</h3>
                     </div>
 
                     <div>
                       <div className="d-flex flex-row text-white align-items-stretch text-center">
-                        
                           <div className="port-item p-4 bg-primary" 
                                 data-target="#home" 
-                                onClick={(i, event)=> {this.toggle(i, event)}} data-event="0">
+                                onClick={(i, event)=> {this.toggle(i, event)}} 
+                                data-event="0">
                               <i className="fa fa-home fa-2x d-block"
-                                onClick={(i, event)=> {this.toggle(i, event)}} data-event="0"
+                                onClick={(i, event)=> {this.toggle(i, event)}} 
+                                data-event="0"
                               ></i>                            
                               <span className="d-none d-sm-block" 
-                                onClick={(i, event)=> {this.toggle(i, event)}} data-event="0">
+                                onClick={(i, event)=> {this.toggle(i, event)}} 
+                                data-event="0">
                                 Home
                               </span>  
                           </div>
                           
                           <div className="port-item p-4 bg-success" 
-                            data-target="#resume">
-                            <i className="fa fa-graduation-cap fa-2x d-block"></i>
-                          <span className="d-none d-sm-block" onClick={(i, event)=> {this.toggle(i, event)}} data-event="1">Resume</span>
+                              data-target="#resume"
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="1">
+                              <i className="fa fa-graduation-cap fa-2x d-block"
+                                 onClick={(i, event)=> {this.toggle(i, event)}} 
+                                 data-event="1">
+                              </i>
+                            <span className="d-none d-sm-block" 
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="1">
+                              Resume
+                              </span>
                           </div>
                           
                           <div className="port-item p-4 bg-warning" 
-                            data-target="#work">
-                            <i className="fa fa-folder-open fa-2x d-block"></i>
-                          <span className="d-none d-sm-block" onClick={(i, event)=> {this.toggle(i, event)}} data-event="2">Work</span>
+                              data-target="#work"
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="2">
+                            <i className="fa fa-folder-open fa-2x d-block"
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="2">
+                            </i>
+                          <span className="d-none d-sm-block" 
+                            onClick={(i, event)=> {this.toggle(i, event)}} 
+                            data-event="2">
+                              Work
+                            </span>
                           </div>
                           
                           <div className="port-item p-4 bg-danger" 
-                            data-target="#contact">
-                            <i className="fa fa-envelope fa-2x d-block"></i>
-                          <span className="d-none d-sm-block" onClick={(i, event)=> {this.toggle(i, event)}} data-event="3">Contact</span>
+                              data-target="#contact"
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="3">
+                            <i className="fa fa-envelope fa-2x d-block" 
+                              onClick={(i, event)=> {this.toggle(i, event)}} 
+                              data-event="3">
+                            </i>
+                          <span className="d-none d-sm-block" 
+                            onClick={(i, event)=> {this.toggle(i, event)}} 
+                            data-event="3">
+                              Contact
+                            </span>
                           </div>
                       </div>
                     </div>      
@@ -128,7 +148,6 @@ class App extends Component {
             </header>
 
            {/* Home */}
-           
            <Collapse isOpen={collapse === 0}>
            <div className="home"> 
               <Card className="bg-primary text-white py-5">
@@ -148,38 +167,61 @@ class App extends Component {
                       jk;aljdfklasj Lorem kjkdflf kleipe Lorem
                     </CardText>
                       
-                      <h4>HTML 5</h4>
-                      <Progress color="success" value="80"/>
-                    
-                      <h4>CSS</h4>
-                      <Progress color="info" value="70"/>
-
-                      <h4>Javascript</h4>
-                      <Progress color="warning" value="65"/>
-
-                      <h4>Python</h4>
-                      <Progress color="danger" value="72"/>
-
-                      <h4>React</h4>
-                      <Progress color="succss" value="61"/>
-
-                      <h4>React Native</h4>
-                      <Progress color="succss" value="61"/>
-
-                       <h4>PostgreSQL</h4>
-                      <Progress color="secondary" value="61"/>
-
-                      <h4>MongoDB</h4>
-                      <Progress color="secondary" value="63"/>
-                         
+                      <div className="move_up">
+                        <h4>HTML 5</h4>
+                        <Progress color="success" value="80">
+                          80%
+                        </Progress>  
+                      </div>
+                      <div className="move_up">
+                        <h4>CSS</h4>
+                        <Progress color="info" value="70">
+                          70%
+                        </Progress>
+                      </div>
+                      <div className="move_up">
+                        <h4>Javascript</h4>
+                        <Progress color="warning" value="72">
+                          72%
+                        </Progress>
+                      </div>
+                      <div className="move_up">
+                        <h4>Python</h4>
+                        <Progress color="danger" value="72">
+                          73%
+                        </Progress>
+                      </div>
+                      <div className="move_up">
+                        <h4>React</h4>
+                        <Progress color="succss" value="61">
+                          61%
+                        </Progress>
+                      </div>  
+                      <div className="move_up">
+                        <h4>React Native</h4>
+                        <Progress color="succss" value="65">
+                          65%
+                        </Progress>
+                      </div>
+                      <div className="move_up">
+                        <h4>PostgreSQL</h4>
+                        <Progress color="secondary" value="61">
+                          61%
+                        </Progress>
+                      </div>
+                      <div className="move_up">  
+                        <h4>MongoDB</h4>
+                        <Progress color="secondary" value="63">
+                          63%
+                        </Progress>
+                      </div>  
                   </CardBody>
                 </Card>
           </Collapse>
           
-
           {/* Resume  */}
           <Collapse isOpen={collapse === 1}>
-            <div className="home"> 
+            <div className="resume"> 
               <Card className="bg-success text-white py-5">
                 <CardBody>
                   <CardTitle>My Resume</CardTitle>
@@ -189,11 +231,75 @@ class App extends Component {
                 </CardBody>
               </Card>
             </div>
+
+             <Card className="py-5">
+                  <CardBody>
+                    <CardTitle>Where Have I Worked?</CardTitle>
+                    <CardText>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, ut!
+                      jk;aljdfklasj Lorem kjkdflf kleipe Lorem
+                    </CardText>
+                    
+                      <Card>
+                        <CardBody>
+                          <CardTitle>Industrial Brothers</CardTitle>
+                          <p className="p-2 mb-3 bg-dark text-white">
+                            Position: Junior Developer
+                          </p>
+                          <CardText>
+                            <li>
+                              Used Python and the Shotgun API to help production managers automate certain tasks.
+                            </li>
+                            <li>
+                              Created a full-stack application for uploading Quicktime and CSV files to be parsed into individual Quicktime files to be uploaded to the Shotgun Database, using Python, Flask, Bootstrap, Shotgun API.
+                            </li>
+                            
+                          </CardText>
+                          <CardFooter className="text-muted">
+                            Dates: Aug. 2016 - Feb. 2018
+                          </CardFooter>
+                        </CardBody>
+                      </Card>
+                      <Card>
+                        <CardBody>
+                          <CardTitle>Corus Entertainment - ​Nelvana Ltd</CardTitle>
+                          <p className="p-2 mb-3 bg-dark text-white">
+                            Position: Technical Artist
+                          </p>
+                          <CardText>
+                            <li>
+                              Scripted Python tools to help artists constrain multiple lights to different cameras as well as help automate other tasks such as a geometry unlock script that switches display types that can be controlled by user by inputting the name of the selected geometry.
+                            </li>   
+                          </CardText>
+                          <CardFooter className="text-muted">
+                            Dates: Aug. 2013 - Jul. 2016
+                          </CardFooter>
+                        </CardBody>
+                      </Card>
+                      <Card>
+                        <CardBody>
+                          <CardTitle>Mr. X VFX</CardTitle>
+                          <p className="p-2 mb-3 bg-dark text-white">
+                            Position: 3D Artist
+                          </p>
+                          <CardText>
+                            <li>
+                              Model, texture and look development for assets in background scenes using Maya, Mari, and Vray
+                            </li>
+                          </CardText>
+                          <CardFooter className="text-muted">
+                            Dates: Jul. 2010 - Jul. 2013
+                          </CardFooter>
+                        </CardBody>
+                      </Card>
+                     
+                  </CardBody>
+                </Card>
           </Collapse>
 
           {/* Work  */}
           <Collapse isOpen={collapse === 2}>
-            <div className="home"> 
+            <div className="work"> 
               <Card className="bg-warning text-white py-5">
                 <CardBody>
                   <CardTitle>Work</CardTitle>
@@ -202,12 +308,38 @@ class App extends Component {
                   </CardText>
                 </CardBody>
               </Card>
+              <Card>
+                <CardBody>
+                  <CardTitle>What Have I Built?</CardTitle>
+                    <CardText>
+                      Model, texture and look development for assets in background scenes using Maya, Mari, and Vray  
+                    </CardText>
+                    <Row noGutters={true}>
+                      <Col md="4">
+                        <a>
+                        <img src={require("./img/person1.jpg")} alt="" className="img-fluid"/>
+                        </a>
+                      </Col>
+                      <Col md="4">
+                        <a>
+                        <img src={require("./img/person1.jpg")} alt="" className="img-fluid"/>
+                        </a>
+                      </Col>
+                      <Col md="4">
+                        <a>
+                        <img src={require("./img/person1.jpg")} alt="" className="img-fluid"/>
+                        </a>
+                      </Col>
+                    </Row>  
+                </CardBody>
+              </Card>
+              
             </div>
           </Collapse>
 
           {/* Contact  */}
           <Collapse isOpen={collapse === 3}>
-            <div className="home"> 
+            <div className="contact"> 
               <Card className="bg-danger text-white py-5">
                 <CardBody>
                   <CardTitle>Contact</CardTitle>
@@ -216,13 +348,21 @@ class App extends Component {
                   </CardText>
                 </CardBody>
               </Card>
+              <Card className="py-5">
+                <CardBody>
+                  <CardTitle>Get In Touch</CardTitle>
+                    <CardText>
+                      <a href={"mailto: blam452@gmail.com"}>blam452@gmail.com</a>
+                    </CardText>
+                  </CardBody>
+                </Card>
             </div>
           </Collapse>
 
           {/* Footer */}
           <footer className="main-footer p-5 bg-dark text-white">
             <Row md="6">
-              <a href=" " className="btn btn-outline-light">
+              <a href="https://drive.google.com/open?id=1KIgLl2IvcEhn9-VK87w3WKB3-EdRpgxy" className="btn btn-outline-light">
                 <i className="fa fa-cloud"></i>
                   Download Resume
               </a>
